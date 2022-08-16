4fe2d3a23ab0b59aac52ec29a3a905c5151a39a9
Neil Ramaswamy <neil@ramaswamy.org>
2022-08-16 09:25:29 -0700

    wow! very very fast

--- mergesort.ts
+++ mergesort.ts
@@ -1,40 +1,24 @@
-// type MergeSort<T extends number[], S extends [number[], number[]] = BetterSplit<T>> =
-//     // Len<T> extends 0 | 1 ? T : Merge<MergeSort<Split<T>[0]>, MergeSort<Split<T>[1]>>
-//     Len<T> extends 0 | 1 ? T : Merge<MergeSort<S[0]>, MergeSort<S[1]>>
+type MergeSort<T extends number[]> =
+    Len<T> extends 0 | 1
+        ? T
+        : Split<T> extends [infer EvenSplit extends number[], infer OddSplit extends number[]] 
+            ? Merge<MergeSort<EvenSplit>, MergeSort<OddSplit>>
+            : T // Will never happen
 
-// Returns an array of numbers that appear at index 0, 2, 4, etc.
-// type GetEvenIndexElements<T extends number[]> =
-//     // Empty array
-//     Same<Len<T>, 0> extends true
-//         ? []
-//         // One element array
-//         : Same<Len<T>, 1> extends true
-//         ? T
-//         // More than one element
-//         : T extends [infer First extends number, infer _ extends number, ...infer Rest extends number[]]
-//         ? [First, ...GetEvenIndexElements<Rest>]
-//         : [] // Cannot happen
+// @ts-ignore-error
+// type Recurse<S extends [number[], number[]]> = S extends [infer A, infer B] ? Merge<MergeSort<A>, MergeSort<B>> : []
 
-// type GetOddIndexElements<T extends number[]> =
-//     IsEmpty<T> extends true
-//         ? []
-//         : Same<Len<T>, 1> extends true
-//         ? []
-//         : T extends [infer _ extends number, infer Second extends number, ...infer Rest extends number[]]
-//         ? [Second, ...GetOddIndexElements<Rest>]
-//         : [] // Cannot happen
-
-type Foo = BetterSplit<[1, 2, 3, 4, 5, 6]>
-
-type BetterSplit<T extends number[]> =
-    IsEmpty<T> extends true ? [[], []]
-        : Same<Len<T>, 1> extends true
-        ? [T, [1]]
-        : T extends [infer First extends number, infer Second extends number, ...infer Rest extends number[]]
-        ? SplitJoinThing<First, Second, BetterSplit<Rest>>
-        : [[], []] // Will never happen
-
-type SplitJoinThing<A extends number, B extends number, C extends [number[], number[]]> =
-    C extends [infer K extends number[], infer T extends number[]]
-        ? [[A, ...K], [B, ...T]]
-        : [[], []] // Won't happen
+// Gives back two arrays
+type Split<
+    T extends number[],
+    Acc1 extends number[] = [],
+    Acc2 extends number[] = []
+> = IsEmpty<T> extends true
+    ? [Acc1, Acc2]
+    : Same<Len<T>, 1> extends true
+    ? [[...Acc1, ...T], Acc2]
+    : T extends [infer First extends number, infer Second extends number, ...infer Rest extends number[]]
+    ? Split<Rest, [...Acc1, First], [...Acc2, Second]>
+    : T extends [infer First extends number, ...infer Rest extends number[]]
+    ? IsEmpty<Rest> extends true ? [[...Acc1, First], Acc2] : [[], []] // Won't happen
+    : [Acc1, Acc2]
