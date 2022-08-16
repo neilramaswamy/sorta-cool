eb5d5baa05225d2ec78d69034f211b9f1cc7e129
Neil Ramaswamy <neil@ramaswamy.org>
2022-08-16 11:42:38 -0700

    a tiny simplificiation

--- mergesort.ts
+++ mergesort.ts
@@ -10,9 +10,7 @@ type Split<
     T extends number[],
     Acc1 extends number[] = [],
     Acc2 extends number[] = []
-> = IsEmpty<T> extends true
-    ? [Acc1, Acc2]
-    : Same<Len<T>, 1> extends true
+> = Len<T> extends 0 | 1
     ? [[...Acc1, ...T], Acc2]
     : T extends [infer First extends number, infer Second extends number, ...infer Rest extends number[]]
     ? Split<Rest, [...Acc1, First], [...Acc2, Second]>
